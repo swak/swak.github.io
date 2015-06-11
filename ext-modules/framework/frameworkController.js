727474cbb3efbfa894b88568445b6dc1945938ea
swak <swak@theswak.com>
2015-06-10 23:23:02 -0700

    Commit this

--- ext-modules/framework/frameworkController.js
+++ ext-modules/framework/frameworkController.js
@@ -1,6 +1,6 @@
 "use strict";
 (function() {
-	angular.module("framework").controller("FrameController",
+	angular.module("framework").controller("FrameworkController",
 		['$scope', '$window', '$timeout', '$rootScope',
 		function ($scope, $window, $timeout, $rootScope) {
 			console.log("Framework Controller Loaded.");
