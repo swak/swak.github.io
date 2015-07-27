52b98bb444c99a0729808938a6c349f9bb8d5a14
swak <swak@theswak.com>
2015-07-26 22:04:34 -0700

    added express

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-'use strict';
+/// <reference path="typings/node/node.d.ts"/>
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var app = express();
 
-var swakApp = angular.module('swakApp', []);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/node_modules', '../node_modules');
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Express server listening at http://%s:%s', host, port);
+});
---
+++ app/appModule.js
@@ -0,0 +1,3 @@
+'use strict';
+
+var swakApp = angular.module('swakApp', []);
